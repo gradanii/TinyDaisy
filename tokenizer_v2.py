c09ebac9cb96ee0451d032697c5cdf658f6e7aa6
Atharva Bhutani <atharvaxcode@proton.me>
2025-04-06 16:29:11 +0530

    The tokenizer is almost complete, just awaiting some final touches

--- tokenizer_v2.py
+++ tokenizer_v2.py
@@ -1,11 +1,40 @@
 import collections
 
-def encoder(string):
-    byte_list = [byte for byte in bytes(string, 'utf-8')]
-    pairs = [tuple(byte_list[i:i+2]) for i in range(0, len(byte_list))]
-    print(byte_list)
-    print(pairs)
-    frequency = dict(collections.Counter(pairs))
-    print(frequency)
 
-encoder('apples were applied')
+def encoder(string):
+    # initialize everything
+    byte_list = list(string.encode("utf-8"))
+    byte_vocab = {bytes([i]).decode("utf-8", errors="ignore"): i for i in range(256)}
+    inv_vocab = dict((v, k) for k, v in byte_vocab.items())
+    new_token = 256
+
+    # find the most frequent pair and add to the byte_vocab
+    while len(byte_list) > 1:
+        pairs = [tuple(byte_list[i : i + 2]) for i in range(len(byte_list) - 1)]
+        frequency = collections.Counter(pairs)
+
+        if frequency.most_common(1)[0][1] == 1:
+            break
+
+        most_freq_pair = frequency.most_common(1)[0][0]
+        word = "".join(inv_vocab[i] for i in most_freq_pair)
+
+        nbyte_list = []
+        byte_vocab[word] = new_token
+        inv_vocab[new_token] = word
+
+        i = 0
+        while i < len(byte_list):
+            pair = tuple(byte_list[i : i + 2])
+            if pair == most_freq_pair:
+                nbyte_list.append(byte_vocab[word])
+                i += 2
+            else:
+                nbyte_list.append(byte_list[i])
+                i += 1
+
+        new_token += 1
+
+        byte_list = nbyte_list
+
+    print(byte_list)
