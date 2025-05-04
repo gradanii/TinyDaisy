0e798c0cae73fc7e093c37036ebc53c902e42f0c
Atharva Bhutani <atharvaxcode@proton.me>
2025-05-04 11:27:56 +0530

    Worked further on self-attention

--- transformer/transformer_np/attention.py
+++ transformer/transformer_np/attention.py
@@ -3,6 +3,11 @@ import numpy as np
 from embeddings import embeddings
 
 
+def softmax(x, axis=-1):
+    exp_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
+
+
 class attention:
     def __init__(self, embed_dim=256, head_dim=64):
         self.embed_dim = embed_dim
@@ -19,3 +24,11 @@ class attention:
         query = embedding @ w_q
         key = embedding @ w_k
         value = embedding @ w_v
+
+        scores = query @ key.T
+
+        scaled_scores = scores / np.sqrt(self.head_dim)
+
+        a = softmax(scaled_scores)
+
+        z = a @ value
