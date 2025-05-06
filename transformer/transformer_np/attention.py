36ee387542b4d7922725f0560e7f0510505ca447
Atharva Bhutani <atharvaxcode@proton.me>
2025-05-06 20:22:07 +0530

    Rewrote self attention

--- transformer/transformer_np/attention.py
+++ transformer/transformer_np/attention.py
@@ -3,32 +3,33 @@ import numpy as np
 from embeddings import embeddings
 
 
-def softmax(x, axis=-1):
-    exp_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
-    return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
-
-
-class attention:
+class SelfAttention:
     def __init__(self, embed_dim=256, head_dim=64):
         self.embed_dim = embed_dim
         self.head_dim = head_dim
+        self.embed = embeddings()
+        self.w_q = np.random.randn(self.embed_dim, self.head_dim)
+        self.w_k = np.random.randn(self.embed_dim, self.head_dim)
+        self.w_v = np.random.randn(self.embed_dim, self.head_dim)
 
-    def self_attention(self, string):
-        w_q = np.random.randn(self.embed_dim, self.head_dim)
-        w_k = np.random.randn(self.embed_dim, self.head_dim)
-        w_v = np.random.randn(self.embed_dim, self.head_dim)
+    @staticmethod
+    def softmax(x, axis=-1):
+        exp_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
 
-        embed = embeddings()
-        embedding = embed.forward(string)
+    def head(self, string):
+        self.embedding = self.embed.forward(string)
 
-        query = embedding @ w_q
-        key = embedding @ w_k
-        value = embedding @ w_v
+        query = self.embedding @ self.w_q
+        key = self.embedding @ self.w_k
+        value = self.embedding @ self.w_v
 
         scores = query @ key.T
 
         scaled_scores = scores / np.sqrt(self.head_dim)
 
-        a = softmax(scaled_scores)
+        a = self.softmax(scaled_scores)
 
         z = a @ value
+
+        return z, a
